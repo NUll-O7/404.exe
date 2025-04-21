826a3d2ff2a3de7d80420d36381ded4e1a5ce322
NUll_O7 <dhruvsonimain@gmail.com>
2025-04-21 11:59:38 +0530

    feat: Update main component with StoryProvider context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { StoryProvider } from './context/StoryContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <StoryProvider>
+      <App />
+    </StoryProvider>
   </StrictMode>,
 )
